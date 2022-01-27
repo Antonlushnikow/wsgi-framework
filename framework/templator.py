69dfc99f212c503e03579a56e5237484b76735a5
antonlushnikow <antonlushnikow@gmail.com>
2022-01-27 20:44:40 +0500

    Added base template

--- framework/templator.py
+++ framework/templator.py
@@ -1,12 +1,12 @@
-import os
-from jinja2 import Template
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
 TEMPLATES = 'templates'
 
 
 def render(template_name, **kwargs):
-    path = os.path.join(TEMPLATES, template_name)
-    with open(path, encoding='utf-8') as f:
-        template = Template(f.read())
+    env = Environment()
+    env.loader = FileSystemLoader(TEMPLATES)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
